2ae1383873402fad5494f92794353c9a6346a3ba
SPECTRA995 <maxmillerasce@gmail.com>
2022-05-10 01:04:02 +0300

    feat: update code

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const keyName = {
-    firstKey:{
+    firstKey: {
         Backquote: '`',
-        Number1: '1',
-        Number2: '2',
-        Number3: '3',
-        Number4: '4',
-        Number5: '5',
-        Number6: '6',
-        Number7: '7',
-        Number8: '8',
-        Number9: '9',
-        Number0: '0',
+        Num1: '1',
+        Num2: '2',
+        Num3: '3',
+        Num4: '4',
+        Num5: '5',
+        Num6: '6',
+        Num7: '7',
+        Num8: '8',
+        Num9: '9',
+        Num0: '0',
         Minus: '-',
         Equal: '=',
-        Backspace: 'backspace',
-        Tab: 'tab',
+        Backspace: 'Backspace',
+        Tab: 'Tab',
         KeyQ: 'q',
         KeyW: 'w',
         KeyE: 'e',
@@ -27,9 +27,9 @@ const keyName = {
         KeyP: 'p',
         BracketLeft: '[',
         BracketRight: ']',
-        LeftSlash: '\\',
-        Delete: 'Del',
-        CapsLock: 'Capslock',
+        BackSlash: '\\',
+        Delete: 'del',
+        CapsLock: 'capslock',
         KeyA: 'a',
         KeyS: 's',
         KeyD: 'd',
@@ -41,8 +41,8 @@ const keyName = {
         KeyL: 'l',
         Semicolon: ';',
         Quote: "'",
-        Enter: 'Enter',
-        ShiftLeft: 'Shift',
+        Enter: 'enter',
+        ShiftLeft: 'shift',
         KeyZ: 'z',
         KeyX: 'x',
         KeyC: 'c',
@@ -52,37 +52,35 @@ const keyName = {
         KeyM: 'm',
         Comma: ',',
         Period: '.',
-        RightSlash: '/',
-        ShiftRight: 'Shift',
-        ControlLeft: 'Ctrl',
-        WinLeft: 'win',
-        AltLeft: 'Alt',
-        Space: 'Space',
-        AltRight: 'Alt',
-        ControlRight: 'Ctrl',
-        ArrowLeft: '←',
+        Slash: '/',
         ArrowUp: '↑',
+        ShiftRight: 'shift ',
+        ControlLeft: 'ctrl',
+        WinLeft: 'win',
+        AltLeft: 'alt',
+        Space: 'space',
+        AltRight: 'alt',
+        ControlRight: 'ctrl',
+        ArrowLeft: '←',
         ArrowDown: '↓',
         ArrowRight: '→',
     }
 };
 
 class Keyboard {
-  constructor(color='grey'){
+  constructor(color='yellow') {
     this.color = color;
     this.wrapper;
     this.content;
     this.textarea;
     this.keyboard;
-    this.keys;
+    this.allKeys;
     this.key;
-    this.transfer;
     this.span;
     this.enter;
+}
 
-  }
-
-creatWrapper(){
+creatWrapper() {
     this.wrapper = document.createElement('div');
     this.wrapper.classList.add('wrapper');
     document.body.append(this.wrapper);
@@ -94,14 +92,13 @@ creatWrapper(){
     this.keyboard = document.createElement('div');
     this.keyboard.classList.add('keyboard', 'keyboard-content');
     this.content.append(this.textarea, this.keyboard);
-    this.keys = document.createElement('div');
-    this.keys.classList.add('keyboard-keys');
+    this.allKeys = document.createElement('div');
+    this.allKeys.classList.add('keyboard-keys');
     this.keyboard.append(this.creatKey(keyName.firstKey));
 }
 
-
-creatKey(obj){
-    for(let [key, value] of Object.entries(obj)){
+creatKey(obj) {
+    for (let [key, value] of Object.entries(obj)) {
         this.key = document.createElement('button');
         this.key.classList.add('keyboard-key');
         this.key.setAttribute('data-key', `${key}`);
@@ -110,165 +107,192 @@ creatKey(obj){
         this.span.setAttribute('data-value', `${value}`);
         this.span.classList.add('keyboard-span');
         this.key.append(this.span);
-        this.keys.append(this.key)
+        this.allKeys.append(this.key);
+       
         if (value === 'del' || value === 'enter') {
             this.span.textContent = value.toUpperCase();
-          } else if(value === 'tab'||value === 'capslock'||value === 'shift'||value === 'alt'||value === 'ctrl'||value === 'win'||value === 'enter'||key === 'ShiftRight' || value === 'backspace'){
-            this.span.textContent =
-              value.slice(0, 1).toUpperCase() + value.slice(1);
+        } else if (value === 'shift' || value === 'alt' || value === 'ctrl' || value === 'win' || value === 'enter' || key === 'ShiftRight' || value === 'Backspace' || value === 'tab' || value === 'capslock') {
+            this.span.textContent = value.slice(0, 1).toUpperCase() + value.slice(1);
           } else {
             this.span.textContent = value.toLowerCase();
           }
-        if (value === 'tab'||value === 'capslock'||value === 'shift'||value === 'alt'||value === 'ctrl'||value === 'win'||value === 'enter'||value === 'del'||value === 'backspace'||key === 'ShiftRight'){
-            this.key.style.background = (this.color)
-        }
 
     }
-    return this.keys
+    return this.allKeys;
 }
 
+};
 
-}
-
-let a = new Keyboard('rgb(201 54 54)');
- a.creatWrapper()
+let test = new Keyboard('rgb(126, 81, 232)');
+test.creatWrapper();
 
 
+const span = document.querySelectorAll(".keyboard-span");
 const button = document.querySelectorAll('.keyboard-key');
+const buttonValue = document.getElementsByClassName('keyboard-key');
 const textarea = document.querySelector('.text-area');
-const span = document.querySelectorAll(".keyboard-span")
 
 
-
-function showValue(event){
+function showValue(event) {
     textarea.value += event.currentTarget.dataset.value;
-  
-}
+};
+
 button.forEach((el) => el.addEventListener('click', showValue));
 
-function buttonSpace(){
-    if(this.dataset.value != "space"){
-        return this.dataset.value
-      } else {
-        return this.dataset.value = " "
+function tabBtn() {
+    if (this.dataset.value != "tab") {
+        return this.dataset.value;
+    } else {
+          return this.dataset.value = "  ";
       }
-}
+};
 
-button.forEach((el) => el.addEventListener('mouseover', buttonSpace));
+button.forEach((el) => el.addEventListener('mouseover', tabBtn));
 
-function buttonTab(){
-    if(this.dataset.value != "tab"){
-        return this.dataset.value
-      } else {
-          return this.dataset.value = "  "
+function backspaceBtn() {
+    if (this.dataset.value != "Backspace") {
+        return this.dataset.value;
+    } else {
+         this.dataset.value = "";
       }
-}
-button.forEach((el) => el.addEventListener('mouseover', buttonTab));
+};
 
-function buttonBackspace(){
-    if(this.dataset.value != "backspace"){
-        return this.dataset.value
-      } else {
-         this.dataset.value = ""
+button.forEach((el) => el.addEventListener('mouseover', backspaceBtn));
+
+function noCapsBtn() {
+    if (this.dataset.value != "capslock") {
+        return this.dataset.value;
+    } else {
+        this.dataset.value = "";
       }
-}
+};
 
-button.forEach((el) => el.addEventListener('mouseover', buttonBackspace));
+button.forEach((el) => el.addEventListener('mouseover', noCapsBtn));
 
-function buttonBackspaceDel(){
-    if(this.dataset.key != "Backspace"){
-        return this.dataset.value
-      } else {
-        let a = textarea.value.toString()
-        return textarea.value = a.slice(0, -1);
+function backspaceDelBtn() {
+    if (this.dataset.key != "Backspace") {
+        return this.dataset.value;
+    } else {
+        let perem = textarea.value.toString();
+        return textarea.value = perem.slice(0, -1);
 
-}
-}
+    }
+};
 
-button.forEach((el) => el.addEventListener('click', buttonBackspaceDel)); 
+button.forEach((el) => el.addEventListener('click', backspaceDelBtn)); 
 
-// function buttonCaps(){
-//     if(this.dataset.key != "CapsLock"){
-//         return this.dataset.value
-//       } else {
-//         console.log(this.button)
-//       }
-// }
-
-// button.forEach((el) => el.addEventListener('click', buttonCaps));
-
-function buttonCapsDel(){
-    if(this.dataset.value != "capslock"){
-        return this.dataset.value
-      } else {
-        this.dataset.value = ""
+function spaceBtn() {
+    if (this.dataset.value != "space") {
+        return this.dataset.value;
+    } else {
+        return this.dataset.value = " ";
       }
-}
-button.forEach((el) => el.addEventListener('mouseover', buttonCapsDel));
+};
 
-function buttonCtrl(){
-    if(this.dataset.value != "ctrl"){
-        return this.dataset.value
-      } else {
-        this.dataset.value = ""
+button.forEach((el) => el.addEventListener('mouseover', spaceBtn));
+
+function altBtn() {
+    if (this.dataset.value != "alt") {
+        return this.dataset.value;
+    } else {
+        this.dataset.value = "";
       }
-}
-button.forEach((el) => el.addEventListener('mouseover', buttonCtrl));
+};
 
-function buttonAlt(){
-    if(this.dataset.value != "alt"){
-        return this.dataset.value
-      } else {
-        this.dataset.value = ""
+button.forEach((el) => el.addEventListener('mouseover', altBtn));
+
+function ctrlBtn() {
+    if (this.dataset.value != "ctrl") {
+        return this.dataset.value;
+    } else {
+        this.dataset.value = "";
       }
-}
-button.forEach((el) => el.addEventListener('mouseover', buttonAlt));
+};
 
-function buttonShiftLeft(){
-    if(this.dataset.key != "ShiftLeft"){
-        return this.dataset.value
-      } else {
-        this.dataset.value = ""
+button.forEach((el) => el.addEventListener('mouseover', ctrlBtn));
+
+function leftShiftBtn() {
+    if (this.dataset.key != "ShiftLeft") {
+        return this.dataset.value;
+    } else {
+        this.dataset.value = "";
       }
-}
-button.forEach((el) => el.addEventListener('mouseover', buttonShiftLeft));
+};
 
-function buttonShiftRight(){
-    if(this.dataset.key != "ShiftRight"){
-        return this.dataset.value
-      } else {
-        this.dataset.value = ""
+button.forEach((el) => el.addEventListener('mouseover', leftShiftBtn));
+
+function rightShiftBtn() {
+    if (this.dataset.key != "ShiftRight") {
+        return this.dataset.value;
+    } else {
+        this.dataset.value = "";
       }
-}
-button.forEach((el) => el.addEventListener('mouseover', buttonShiftRight));
+};
 
-function buttonWin(){
-    if(this.dataset.value != "win"){
-        return this.dataset.value
-      } else {
-        this.dataset.value = ""
+button.forEach((el) => el.addEventListener('mouseover', rightShiftBtn));
+
+function winBtn() {
+    if (this.dataset.value != "win") {
+        return this.dataset.value;
+    } else {
+        this.dataset.value = "";
       }
-}
-button.forEach((el) => el.addEventListener('mouseover', buttonWin));
+};
 
-function buttoEnter(){
-    if(this.dataset.key != "Enter"){
-        return this.dataset.value 
-      } else { 
-          textarea.value += '\r\n'
+button.forEach((el) => el.addEventListener('mouseover', winBtn));
 
+function enterBtn() {
+    if (this.dataset.key != "Enter") {
+        return this.dataset.value;
+    } else { 
+          textarea.value += '\r\n';
       }
-}
+};
 
-button.forEach((el) => el.addEventListener('click', buttoEnter))
+button.forEach((el) => el.addEventListener('click', enterBtn));
 
-function buttoEnterDel(){
-    if(this.dataset.value != "enter"){
-        return this.dataset.value 
-      } else { 
-        this.dataset.value = ""
+function noEnterBtn() {
+    if (this.dataset.value != "enter") {
+        return this.dataset.value; 
+    } else { 
+        this.dataset.value = "";
       }
-}
+};
 
-button.forEach((el) => el.addEventListener('mouseover', buttoEnterDel))
+button.forEach((el) => el.addEventListener('mouseover', noEnterBtn));
 
+function delButton() {
+  if (this.dataset.value != "del") {
+      return this.dataset.value; 
+  } else { 
+      this.dataset.value = "";
+    }
+};
+
+button.forEach((el) => el.addEventListener('mouseover', delButton));
+
+
+
+function moveDown() {
+  if (this.dataset.key != "Enter" && this.dataset.key != "Backspace" && this.dataset.key != "Delete" && this.dataset.key != "ShiftRight" && this.dataset.key != "Tab" && this.dataset.key != "CapsLock" && this.dataset.key != "ShiftLeft" && this.dataset.key != "ControlLeft" && this.dataset.key != "WinLeft" && this.dataset.key != "AltLeft" && this.dataset.key != "Space" && this.dataset.key != "AltRight" && this.dataset.key != "ControlRight") {
+  this.style.transform = 'scale(1.1)';
+  this.style.backgroundColor = 'rgb(201, 214, 21)';
+  } else {
+    this.style.borderRadius = 'scale(1.1)';
+    this.style.backgroundColor = 'rgb(231, 67, 240)';
+  }
+};
+
+button.forEach((el) => el.addEventListener('mousedown', moveDown));
+
+function moveUp() {
+  if (this.dataset.key != "Enter" && this.dataset.key != "Backspace" && this.dataset.key != "Delete" && this.dataset.key != "ShiftRight" && this.dataset.key != "Tab" && this.dataset.key != "CapsLock" && this.dataset.key != "ShiftLeft" && this.dataset.key != "ControlLeft" && this.dataset.key != "WinLeft" && this.dataset.key != "AltLeft" && this.dataset.key != "Space" && this.dataset.key != "AltRight" && this.dataset.key != "ControlRight") {
+    this.style.transform = 'none';
+    this.style.backgroundColor = 'rgb(231, 67, 240)';
+  } else {
+      this.style.transform = 'none';
+      this.style.backgroundColor = 'rgb(201, 214, 21)';
+    }
+};
+
+button.forEach((el) => el.addEventListener('mouseup', moveUp));
